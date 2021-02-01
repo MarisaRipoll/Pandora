63a74d0e580548e225fb8e00e3b99fa43fc58c3e
Marisa <marisaripoll218@gmail.com>
2021-02-01 13:31:07 +0100

    default must be batch_size=1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ path1 = 'data/squad/train-v2.0.json'
 path2 = 'data/squad/dev-v2.0.json'
 tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 model = DistilBertForQuestionAnswering.from_pretrained("distilbert-base-uncased")
-batch_size = 2
+batch_size = 1
 verbose = False
 optim = AdamW(model.parameters(), lr=5e-5)
 
--- setup4L.py
+++ setup4L.py
@@ -8,7 +8,7 @@ path1 = 'data/squad/train-v2.0.json'
 path2 = 'data/squad/dev-v2.0.json'
 tokenizer = LongformerTokenizerFast.from_pretrained('allenai/longformer-base-4096')
 model = LongformerForQuestionAnswering.from_pretrained('allenai/longformer-base-4096')
-batch_size = 2
+batch_size = 1
 verbose = True
 optim = AdamW(model.parameters(), lr=5e-5)
 
