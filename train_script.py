5fd1dd14b3cdb96ba98ff381892aa0b3716d2a20
Marisa <marisaripoll218@gmail.com>
2021-02-01 13:47:50 +0100

    train_script update

--- train_script.py
+++ train_script.py
@@ -18,6 +18,7 @@ def train(train_loader, val_loader, model_type='longformer', optim=0, lr=5e-5, m
     if optim == 0: optim = AdamW(model.parameters(), lr=lr)
     summary_path = f'runs/{model_type}_{len(train_loader)}samples_{num_epochs}epochs_{lr}lr_{datetime.now().strftime("%b-%d-%Y-%H%M%S")}'
     writer = SummaryWriter(summary_path)
+    model = model.to(device)
 
     em_scores_train = []
     f1_scores_train = []
@@ -41,22 +42,8 @@ def train(train_loader, val_loader, model_type='longformer', optim=0, lr=5e-5, m
             # global_attention_mask = torch.zeros(input_ids.shape, dtype=torch.long, device=input_ids.device)
             # outputs = model(input_ids, attention_mask=attention_mask, global_attention_mask=global_attention_mask, output_attentions=True, start_positions=start_positions, end_positions=end_positions)
             optim.zero_grad()
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            start_positions = batch['start_positions'].to(device)
-            end_positions = batch['end_positions'].to(device)
-            outputs = model(input_ids, attention_mask=attention_mask, output_attentions=True, start_positions=start_positions, end_positions=end_positions)
-            nbest = likeliest_predictions(outputs.start_logits, outputs.end_logits, batch['input_ids'], tokenizer, n=5)
-            all_tokens = tokenizer.convert_ids_to_tokens(input_ids[0].tolist())
-            target_tokens = all_tokens[start_positions:end_positions+1]
-            target_text = tokenizer.decode(tokenizer.convert_tokens_to_ids(target_tokens))
-            em_score = max((em_metric(nbest[i].text, target_text)) for i in range(len(nbest)))
-            f1_score = max((f1_metric(nbest[i].text, target_text)) for i in range(len(nbest)))
-            em_score_epoch.append(em_score)
-            f1_score_epoch.append(f1_score)
-            loss = outputs.loss
-            #loss, f1_score, em_score = get_scores(batch, em_score_epoch_val, f1_score_epoch_val,
-            #                                      device, tokenizer, model)
+            loss, f1_score, em_score = get_scores(batch, em_score_epoch_val, f1_score_epoch_val,
+                                                  device, tokenizer, model)
             writer.add_scalar("Loss/train", loss, epoch)
             writer.add_scalar("f1_score/train", f1_score, epoch)
             writer.add_scalar("em_score/train", em_score, epoch)
@@ -73,22 +60,8 @@ def train(train_loader, val_loader, model_type='longformer', optim=0, lr=5e-5, m
             ##### EVALUATION STEP #####
             ###########################
 
-            #loss, f1_score, em_score = get_scores(batch, em_score_epoch_val, f1_score_epoch_val,
-            #                                      device, tokenizer, model)
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            start_positions = batch['start_positions'].to(device)
-            end_positions = batch['end_positions'].to(device)
-            outputs = model(input_ids, attention_mask=attention_mask, output_attentions=True, start_positions=start_positions, end_positions=end_positions)
-            nbest = likeliest_predictions(outputs.start_logits, outputs.end_logits, batch['input_ids'], tokenizer, n=5)
-            all_tokens = tokenizer.convert_ids_to_tokens(input_ids[0].tolist())
-            target_tokens = all_tokens[start_positions:end_positions+1]
-            target_text = tokenizer.decode(tokenizer.convert_tokens_to_ids(target_tokens))
-            em_score = max((em_metric(nbest[i].text, target_text)) for i in range(len(nbest)))
-            f1_score = max((f1_metric(nbest[i].text, target_text)) for i in range(len(nbest)))
-            em_score_epoch.append(em_score)
-            f1_score_epoch.append(f1_score)
-            loss = outputs.loss
+            loss, f1_score, em_score = get_scores(batch, em_score_epoch_val, f1_score_epoch_val,
+                                                  device, tokenizer, model)
             writer.add_scalar("f1_score/val", f1_score, epoch)
             writer.add_scalar("em_score/val", em_score, epoch)
             print(f'Step {i} - f1_score: {f1_score:.3}')
